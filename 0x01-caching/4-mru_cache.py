c21b0bc89a478e54c7266d563880e6dc09fd3c8d
Razig17 <abdelrazig.sharif1@gmail.com>
2024-09-03 22:21:39 +0300

    implement MRU caching policy

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+MRUCache module
+"""
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    MRUCache class that inherits from BaseCaching
+    """
+
+    def __init__(self):
+        """
+        Initiliaze
+        """
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """
+        Add an item in the cache
+        """
+        if key and item:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                if self.queue:
+                    removed = self.queue.pop(-1)
+                    del self.cache_data[removed]
+                    print("DISCARD: {}".format(removed))
+            self.cache_data[key] = item
+            self.queue.append(key)
+
+    def get(self, key):
+        """
+        Get an item by key
+        """
+        if key in self.cache_data:
+            self.queue.remove(key)
+            self.queue.append(key)
+        return self.cache_data.get(key, None)
